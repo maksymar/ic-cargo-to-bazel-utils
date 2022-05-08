d4935be2e6fb8e6f64287fd7992c8d9c8803112d
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2022-05-08 17:17:31 +0200

    add graphviz

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import copy
 import toml
+import graphviz
 from pathlib import Path
 
 
@@ -65,11 +66,9 @@ def mark_subtree(graph, current, target, path, is_found=False):
 def remove_unwanted_nodes(graph):
     subtree = {}
     for package_name in graph:
-        if graph[package_name].get('traversing_status') != 'found':
-            continue
-        entry = copy.deepcopy(graph[package_name])
-        del entry['traversing_status']
-        subtree[package_name] = entry
+        if graph[package_name].get('traversing_status') == 'found':
+            subtree[package_name] = copy.deepcopy(graph[package_name])
+            del subtree[package_name]['traversing_status']
     return subtree
 
 
@@ -103,15 +102,98 @@ def calculate_progress(graph):
     return (bazel_n, total, ratio)
 
 
+def add_height(graph, current):
+    info = graph.get(current)
+    height = -1
+    # Skip packages with Bazel.
+    if info is None or info.get('bazel_path') is not None:
+        return height
+    for child in info.get('children', []):
+        height = max(height, add_height(graph, child))
+    info['height'] = height + 1
+    return info['height']
+
+
+def interpolate_rgb(rgb_lo, rgb_hi, param):
+    def interpolate(a, b, param):
+        return int(a * (1 - param) + b * param)
+
+    r = interpolate(rgb_lo[0], rgb_hi[0], param)
+    g = interpolate(rgb_lo[1], rgb_hi[1], param)
+    b = interpolate(rgb_lo[2], rgb_hi[2], param)
+    return f'#{r:02X}{g:02X}{b:02X}'
+
+
+def add_height_color(graph, color_lo, color_hi):
+    max_height = max([graph[x].get('height', -1) for x in graph])
+    if max_height is None or max_height <= 0:
+        return
+    for package_name in graph:
+        info = graph[package_name]
+        height = info.get('height')
+        if height is None:
+            continue
+        param = height / max_height
+        info['color'] = interpolate_rgb(color_lo, color_hi, param)
+
+
+def to_graphviz(graph):
+    nodes_n = len(graph.keys())
+    edges_n = sum([len(graph[x].get('children', [])) for x in graph])
+    print(f'Plotting {nodes_n} nodes with {edges_n} edges...')
+
+    dot = graphviz.Digraph()
+
+    # Create nodes.
+    for package_name in graph.keys():
+        node_text = f'{package_name}'
+        fillcolor = 'grey'  # default
+
+        # Display height.
+        height = graph[package_name].get('height')
+        if height is not None:
+            node_text += f'\nheight:{height}'
+
+        # Display bazel status and color.
+        bazel_path = graph[package_name].get('bazel_path')
+        if bazel_path:
+            node_text += f'\nbazel:yes'
+            fillcolor = 'green'
+
+        # Display not converted node color.
+        color = graph[package_name].get('color')
+        if color is not None:
+            #node_text += f'\ncolor:{color}'
+            fillcolor = color
+
+        dot.node(package_name, node_text, style='filled', fillcolor=fillcolor)
+
+    # Create edges.
+    for package_name in graph.keys():
+        for child in graph[package_name].get('children', []):
+            dot.edge(package_name, child)
+
+    # print(dot.source)  # DEBUG
+    return dot
+
+
 def main():
     SOURCE_DIR = '../ic/rs/'
-    ROOT_PACKAGE = None  # 'ic-types'
+    ROOT_PACKAGE = 'ic-types'
 
     graph = build_graph(SOURCE_DIR)
     subtree = extract_subtree(graph, ROOT_PACKAGE)
     bazel_n, total, ratio = calculate_progress(subtree)
     print(
         f'Packages converted to Bazel: {bazel_n} / {total} ({100*ratio:>5.01f}%)')
+
+    add_height(subtree, ROOT_PACKAGE)
+    RED = (255, 0, 0)
+    YELLOW = (255, 255, 0)
+    add_height_color(subtree, RED, YELLOW)
+
+    dot = to_graphviz(subtree)
+    dot.render('./graph.gv', view=True)
 
 
 if __name__ == '__main__':
