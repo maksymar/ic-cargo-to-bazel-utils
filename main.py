7e820fbc529c78ef7ed6b7b42b06ac15690e9bf2
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2022-05-08 16:53:21 +0200

    add progress

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def read(path):
         return f.read()
 
 
-def build_graph(source_folder):
+def build_graph(source_dir):
     # Collect Cargo.toml paths.
     data = [
         {
@@ -19,7 +19,7 @@ def build_graph(source_folder):
             'cargo_toml': toml.loads(read(path)),
             'bazel_path': None,
         }
-        for path in Path(source_folder).rglob('Cargo.toml')
+        for path in Path(source_dir).rglob('Cargo.toml')
     ]
 
     # Collect BUILD.bazel paths.
@@ -45,6 +45,36 @@ def build_graph(source_folder):
             'children': children,
         }
 
+    return graph
+
+
+def mark_subtree(graph, current, target, path, is_found=False):
+    info = graph.get(current)
+    if info is None or info.get('traversing_status') == 'found':
+        return
+    path += [current]  # Add current to path.
+    if info.get('traversing_status') == 'searching':
+        raise ValueError(f'Unexpected graph cycle, see path: {path}')
+    info['traversing_status'] = 'searching'
+    is_found = is_found or current == target
+    for child in info.get('children', []):
+        mark_subtree(graph, child, target, path, is_found)
+    info['traversing_status'] = 'found' if is_found else 'not-found'
+    path.pop()         # Remove current from path.
+
+
+def remove_unwanted_nodes(graph):
+    subtree = {}
+    for package_name in graph:
+        if graph[package_name].get('traversing_status') != 'found':
+            continue
+        entry = copy.deepcopy(graph[package_name])
+        del entry['traversing_status']
+        subtree[package_name] = entry
+    return subtree
+
+
+def filter_subtree(graph, target_package):
     # Get roots.
     roots = set(graph.keys())
     for package_name in graph:
@@ -56,14 +86,33 @@ def build_graph(source_folder):
     graph[FAKE_ROOT] = {'children': roots}
     print(f'Root nodes linked to "{FAKE_ROOT}": {len(roots)}')
 
-    return graph
+    if target_package is None or len(str(target_package).strip()) == 0:
+        return graph
+
+    # Extract target package subtree.
+    path = []
+    mark_subtree(graph, FAKE_ROOT, target_package, path)
+    subtree = remove_unwanted_nodes(graph)
+
+    return subtree
+
+
+def calculate_progress(graph):
+    bazel_n = sum([1 for x in graph if graph[x].get('bazel_path') is not None])
+    total = len(graph.keys())
+    ratio = bazel_n / total
+    return (bazel_n, total, ratio)
 
 
 def main():
-    ROOT = '../ic/rs/'
+    SOURCE_DIR = '../ic/rs/'
+    ROOT_PACKAGE = None  # 'ic-types'
 
-    graph = build_graph(ROOT)
-    # print(graph)
+    graph = build_graph(SOURCE_DIR)
+    subtree = filter_subtree(graph, ROOT_PACKAGE)
+    bazel_n, total, ratio = calculate_progress(subtree)
+    print(
+        f'Packages converted to Bazel: {bazel_n} / {total} ({100*ratio:>5.01f}%)')
 
 
 if __name__ == '__main__':
